9c8e717d6b60747d118dd213ac3df4473515ea77
meccar <tung14hn@gmail.com>
2024-08-04 22:15:28 +0800

    updated files

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,7 +1,20 @@
 import { MessagesRepository } from './messages.repository';
+
 export class MessagesService {
   messagesRepository: MessagesRepository;
   constructor() {
     this.messagesRepository = new MessagesRepository();
+  }
+
+  async findOne(id: string) {
+    return this.messagesRepository.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepository.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepository.create(content);
   }
 }
